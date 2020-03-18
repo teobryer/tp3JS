d0a9a9a223b85f9530d0022303f59dc7a8dd2e1d
teobryer <teocalme@gmail.com>
2020-03-18 17:45:37 +0100

    add row to table

--- exercice-1/notes.js
+++ exercice-1/notes.js
@@ -1,1 +1,38 @@
 // A COMPLETER
+
+document.getElementById("ajout").addEventListener("click", ajouterLigne);
+
+
+
+
+
+
+function ajouterLigne(){
+	
+	var numeroEtu = document.querySelector("input[type='text']").value;
+	var table = document.getElementsByTagName("table")[0].childNodes[1];
+	
+	var tr = document.createElement('tr');
+    table.appendChild(tr);
+     
+    var td = document.createElement('td');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    
+    
+    tr.appendChild(td);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    
+    var tdText = document.createTextNode(numeroEtu);
+    td.appendChild(tdText);
+    td1.appendChild(tdText);
+    td2.appendChild(tdText);
+    td3.appendChild(tdText);
+  
+	
+	
+	
+}
